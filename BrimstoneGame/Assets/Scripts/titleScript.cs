b75bb7a6ab1557619568a63a2e268aeeb157c4f2
sadathalam <sadathalam98@gmail.com>
2021-04-19 15:41:48 -0400

    TitleScreen and LevelUpdate

---
+++ BrimstoneGame/Assets/Scripts/titleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class titleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public Button startButton;
+    public Button quitButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener((UnityEngine.Events.UnityAction)this.startButtonClick);
+        quitButton.onClick.AddListener((UnityEngine.Events.UnityAction)this.quitButtonClick);
+
+    }
+    void Update()
+    {
+        
+    }
+   void startButtonClick()
+    {
+        Application.LoadLevel("Level1");
+    }
+    
+    void quitButtonClick()
+    {
+        Application.Quit(0);
+    }
+}
